1c559d35bb0b453c09ba19f5b5b5195aa6558d27
Anastasiya Cherkasova <cherkasova2000@yandex.ru>
2020-10-27 13:29:45 +0300

    readyLab2

---
+++ lab2/circleController.js
@@ -0,0 +1,22 @@
+var Controller = function (View, Model) {
+    this.circleView = View;
+    this.circleModel = Model;
+};
+
+Controller.prototype.init = function () {
+  this.circleView.init();
+  this.circleModel.init();
+};
+
+function func() {
+    let forRemove = []
+    this.circleModel.changeSize(forRemove)
+    this.circleView.renderAll(this.circleModel.objs, forRemove);
+    this.circleModel.removeObjs(forRemove)
+    var last = this.circleModel.generateCirсle()   
+    this.circleView.renderOne(last)
+}
+
+var circleController = new Controller(circleView, circleModel);
+circleController.init();
+setInterval(func, 500)
---
+++ lab2/circleModel.js
@@ -0,0 +1,64 @@
+
+var Model = function () {
+    this.objs = [];
+};   
+
+
+Model.prototype.init = function(){
+};
+
+Model.prototype.generateCirсle = function () {
+    let _obj = {
+        x: getRandomFloat(0, 1000),
+        y: getRandomFloat(0, 600),
+        r: getRandomFloat(20, 80),
+        R: 0,
+        color: getRandomColor()
+    }
+    _obj.R = _obj.r
+    this.objs.push(_obj)
+    return _obj
+}
+
+Model.prototype.changeSize = function(forRemove) {
+    for(var i = 0; i < this.objs.length; i++) {
+        this.objs[i].r += 5;
+        if (this.objs[i].r > this.objs[i].R * 2) {
+            forRemove.push(i)
+        }
+    }
+}
+
+Model.prototype.removeObjs = function(forRemove) {
+    var removed = []
+    for (var i = 0; i < forRemove.length; i++) {
+        removed.push(this.objs[forRemove[i]])   
+    }
+    for (var i = 0; i < removed.length; i++) {
+        for (var j = 0; j < this.objs.length; j++) {
+            if (removed[i] === this.objs[j]) {
+                this.objs.splice(j, 1)
+                break
+            }
+        }
+    }  
+};
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var circleModel = new Model()
+
+
+
